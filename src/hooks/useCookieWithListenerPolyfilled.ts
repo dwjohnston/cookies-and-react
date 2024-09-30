4432257caa2638392bdad2e27bba043de97f2df1
David Johnston <david@blacksheepcode.com>
2024-09-30 17:54:46 +1000

    Comments, whitespace clean up

--- src/hooks/useCookieWithListenerPolyfilled.ts
+++ src/hooks/useCookieWithListenerPolyfilled.ts
@@ -9,6 +9,7 @@ type UseCookieReturn<T> = [
 ];
 
 
+// Polyfilled cookieStore wrappers. 
 async function getCookie(cookieName: string) : Promise<string | null> {
 
   if("cookieStore" in window) {
@@ -33,11 +34,11 @@ async function deleteCookie(cookie: string) : Promise<void> {
   } 
 
   Cookies.remove(cookie);
-
 }
 
 function listenForCookieChange(cookieName: string, onChange: (newValue: string | null) => void) : (() => void) {
 
+  // If the cookieStore is available, we can can use the change event listener
   if("cookieStore" in window) {
     const changeListener = (event) => {
       console.log(event)
@@ -56,11 +57,13 @@ function listenForCookieChange(cookieName: string, onChange: (newValue: string |
 
     cookieStore.addEventListener("change", changeListener);
 
+    // We return a clean up function for the effect
     return () => {
       cookieStore.removeEventListener("change", changeListener);
     };
   }
 
+  // If cookieStore is not available, we poll for changes. 
   const interval = setInterval(() => {
     const cookie = Cookies.get(cookieName);
     if(cookie) {
@@ -70,6 +73,8 @@ function listenForCookieChange(cookieName: string, onChange: (newValue: string |
     }
   }, 1000);
 
+
+  // We still return a clean up function for the effect
   return () => {
     clearInterval(interval);
   }
@@ -101,8 +106,6 @@ export default function useCookieWithListener<T>(
       }
     });
 
-
-
     // Any subsequent changes to the cookie will be listened to
     // and set into state
     return listenForCookieChange(name, (newValue) => {
@@ -113,7 +116,6 @@ export default function useCookieWithListener<T>(
       }
     });
   }, [name]);
-
 
   // For updates to the cookie we just update the cookie store directly,
   // And allow the event listener to update the state
